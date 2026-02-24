4387a14c5e572bcafd566d7d3cab9f78367d9e9c
2lkoki <wichayatugtung@gmail.com>
2026-02-25 00:07:28 +0700

    hello

--- main.cpp
+++ main.cpp
@@ -150,7 +150,7 @@ int main() {
         } else if (choice==3) {
 
         } else if (choice==4) {
-            //Crad ใส่ตรงนี้ อั้นนี้ของ Card!!!
+            //Crad ใส่ตรงนี้ อั้นนี้ของ Card!!! ดีจ้าาาาา
         } else if (choice==5) {
 
         } else if (choice==0) {
